61333196d59e89bc32716c9f8044bb473a706998
MatheusBenetti <mbbenetti1@gmail.com>
2024-08-03 15:11:41 -0300

    first commit

---
+++ src/test/java/matheus/benetti/socialnetwork/SocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.benetti.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
